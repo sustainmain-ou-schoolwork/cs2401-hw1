19b251db9ebc00236e50fdfc73aaa9e3e71e61ea
Josh Marusek <jm553020@ohio.edu>
2022-02-05 16:35:54 -0500

    add Checkbook::save

--- checkbook.cc
+++ checkbook.cc
@@ -9,6 +9,7 @@ Checkbook::Checkbook() {
     balance = 0;
 }
 
+
 // modifier
 
 /**
@@ -28,7 +29,7 @@ void Checkbook::write_check(std::istream& ins) {
 
     // set the check number if it wasn't previously set
     if (&ins == &cin) {
-        tmp.set_check_num(used);
+        tmp.set_check_num(nextCheckNum);
     }
 
     // add the check to the checkbook
@@ -41,7 +42,10 @@ void Checkbook::remove(size_t checkNum) {
 
 }
 
+
 // sorter
+
+
 void Checkbook::number_sort() {
 
 }
@@ -54,16 +58,35 @@ void Checkbook::date_sort() {
 
 }
 
+
 // file operation
+
+
 void Checkbook::load_from_file(std::ifstream& fileStream) {
 
 }
 
-void Checkbook::save(std::ofstream& fileStream) const {
 
+/**
+ * @brief Outputs the checkbook data to an ostream.
+ * 
+ * @param fileStream the stream to output checkbook data to
+ */
+void Checkbook::save(std::ofstream& fileStream) const {
+    fileStream << balance << '\n';
+    fileStream << nextCheckNum << '\n';
+    
+    for (size_t i = 0; i < used; i++) {
+        fileStream << checks[i];
+    }
+    
+    fileStream << flush;
 }
 
+
 // display or calculate info
+
+
 void Checkbook::show_all(std::ostream& outs) const {
 
 }
